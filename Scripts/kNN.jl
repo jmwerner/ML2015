d53693145ed9aa65de0932c14eca97f1275d7a97
Jeremy Werner <jeremy@jerwerner.com>
2015-01-19 23:19:22 -0800

    Updating kNN to include higher dimensional classification via PCA and other misc. improvements

--- Scripts/kNN.jl
+++ Scripts/kNN.jl
@@ -3,28 +3,50 @@
 # Begin with naive 2 dimensional data set (with the intent of eventually expanding to n dimensional data sets)
 
 using RDatasets
+using MultivariateStats
 
 iris = dataset("datasets", "iris")
 
 # PetalLength and PetalWidth will be used for 2D example
 
-function euclidean_distance(vector1, vector2)
+function euclidean_distance(vector1::Array, vector2::Array)
 	diff = vector1 - vector2
 	sqrt(sum(diff .* diff))
 end # distance
 
 function voting_function(vector_in::Array)
 	counts_map = countmap(vector_in)
-	indicator = sortperm(collect(values(counts_map)), rev = true)[1]
-	collect(keys(counts_map))[indicator]
+	vals = collect(values(counts_map))
+	indicators = sortperm(vals, rev = true)
+	if(length(indicators) > 1)
+		if(vals[indicators[1]] == vals[indicators[2]])
+			warn("Voting Tie! Please select k that is not a multiple of the number of output classes")
+		end
+	end
+	collect(keys(counts_map))[indicators[1]]
 end
 
 # Returns array of categorical predictions for the predict_matrix input
 function kNN(X::Matrix, y::Array, k::Integer, predict_matrix::Matrix, distance_metric::Function, voting_scheme::Function)
-	N = size(predict_matrix)[1]
+	N = size(predict_matrix, 1)
 	predictions = Array(ASCIIString, N)
+	
+	if(ndims(X) > 1)
+		if(size(X,2) > 2)
+		 	pca_model = fit(PCA, transpose(X))
+		 	X_array = transpose(transform(pca_model, transpose(X)))
+		 	predict_array = transpose(transform(pca_model, transpose(predict_matrix)))
+		else
+			X_array = X
+			predict_array = predict_matrix
+		end
+	else
+		X_array = X
+		predict_array = predict_matrix
+	end
+
 	for i = 1:N
-		distances = map(j -> distance_metric(X[j,:], predict_matrix[i,:]), 1:(size(X)[1]))
+		distances = map(j -> distance_metric(X_array[j,:], predict_array[i,:]), 1:(size(X_array)[1]))
 		indexes = sortperm(distances)
 		predictions[i] = voting_scheme(y[indexes[1:k]])
 	end
@@ -35,6 +57,7 @@ end # kNN
 ##########
 # Driver #
 ##########
+
 x_all = hcat(iris[:PetalLength], iris[:PetalWidth])
 
 subset_n = 120
@@ -58,5 +81,22 @@ class_3 = sum(real_y .== test_predictions_3) / n1
 class_4 = sum(real_y .== test_predictions_4) / n1
 class_5 = sum(real_y .== test_predictions_5) / n1
 class_6 = sum(real_y .== test_predictions_6) / n1
+
+
+
+
+# Higher dimensional test
+
+x_all = hcat(iris[:PetalLength], iris[:PetalWidth], iris[:SepalLength], iris[:SepalWidth])
+
+subset_n = 120
+x_train = x_all[1:subset_n, 1:4]
+x_test = x_all[(subset_n + 1):150, 1:4]
+y_in = array(iris[:Species][1:subset_n])
+
+test_predictions_3 = kNN(x_train, y_in, 3, x_test, euclidean_distance, voting_function)
+class_3_big = sum(real_y .== test_predictions_3) / n1
+
+
 
 
--- Scripts/kNN.md
+++ Scripts/kNN.md
@@ -1,11 +1,11 @@
 kNN Readme
 =====
 
-This script will run a kNN classification and predict classification outputs for a 2-dimensional data set in its current state.  
+This script will run a kNN classification algorithm and predict classification outputs for data sets. Currently, it only performs PCA to reduce dimensionality if data with more than 2 columns is given. It will reduce the data to the default number of components returned by PCA from the `MultivariateStats` module. 
 
 To Do
 -----
-1. Adapt for higher than 2-dimensional data sets
-2. Fix ties in both the voting function and distance function (add warning messages as well)
-3. Clean up specific data types for inputs/outputs of functions
+1. ~~Adapt for higher than 2-dimensional data sets~~
+2. ~~Fix ties in both the voting function and distance function - left distance for now because of rarity (add warning messages as well)~~
+3. ~~Clean up specific data types for inputs/outputs of functions~~
 4. Add execution examples (example is currently embeded in the script itself)
